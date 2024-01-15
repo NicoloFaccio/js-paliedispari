2d7062e4b41b6f79a089b76032bbb5ce3418c8bc
NicoloFaccio <nicolo.faccio1@gmail.com>
2024-01-15 16:47:42 +0100

    created the constants

---
+++ Pari e dispari/assets/JS/main.js
@@ -0,0 +1,15 @@
+//L’utente sceglie pari o dispari e inserisce un numero da 1 a 5.
+//Generiamo un numero random (sempre da 1 a 5) per il computer (usando una funzione).
+//Sommiamo i due numeri
+//Stabiliamo se la somma dei due numeri è pari o dispari (usando una funzione)
+//Dichiariamo chi ha vinto.
+
+const oddEven = prompt("Scegliere pari o dispari:")
+const userNumber = parseInt(prompt("Inserire un numero da 1 a 5:"))
+
+console.log(oddEven, userNumber)
+
+//function numberPc (){
+//    Math.floor(Math.random() * 5);
+//}
+
